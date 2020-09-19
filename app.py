ae03cc7411c7848dc4b07a109848b02f401920e1
claumitdia <mitchellemoras@gmail.com>
2020-09-19 17:06:45 +0530

    only god knows what im doing

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def helloWorld():
     return 'Hello World. Fucking shit!'
 
+@app.route('/<name>')
+def helloName(name):
+    return 'Hello {0}'.format(name) 
+
 if __name__=='__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run(host='0.0.0.0', port=9000, debug=True)
+    
