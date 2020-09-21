4e3b55d65241ba1a5885b756a7fd38edd3efc349
claumitdia <mitchellemoras@gmail.com>
2020-09-21 22:55:04 +0530

    Committing word count project using nltk

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this_really_needs_to_be_changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
